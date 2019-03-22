a596893e7a9ced58dcab3a83ca24ae5008afee19
Mikulew <mikulew@gmail.com>
2019-03-22 19:55:03 +0100

    Add header logo and rename component HelloWorld to Header

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img class="super-heroes" alt="superheroes image" src="./assets/super-heroes.jpg">
+    <Header msg="Super herores"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue';
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Header,
   }
 }
 </script>
@@ -24,5 +24,10 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.super-heroes {
+  width: auto;
+  height: 200px;
 }
 </style>
