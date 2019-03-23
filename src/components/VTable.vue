6bf0ff209de0b3985f53dcb02f99f4ef619702bb
Mikulew <mikulew@gmail.com>
2019-03-23 20:56:21 +0100

    Add Vtable in SuperheroesList

---
+++ src/components/VTable.vue
@@ -0,0 +1,43 @@
+<template>
+    <table class="table table-sm">
+        <thead>
+            <tr>
+                <th>#</th>
+                <th>Full Name</th>
+                <th>Alter Egos</th>
+                <th>Lorem ipsum</th>
+                <th>Actions</th>
+            </tr>
+        </thead>
+
+        <tbody>
+            <tr>
+                <th scope="row">1</th>
+                <td>Bruce Wayne</td>
+                <td>Batman</td>
+                <td>"I have a batcard!"</td>
+                <td>
+                    <button type="button" class="btn btn-outline-primary">Button</button>
+                </td>
+            </tr>
+            <tr>
+                <th scope="row">2</th>
+                <td>Peter Parker</td>
+                <td>Spiderman</td>
+                <td>"Everybody likes me!"</td>
+                <td>
+                    <button type="button" class="btn btn-outline-primary">Button</button>
+                </td>
+            </tr>
+            <tr>
+                <th scope="row">3</th>
+                <td>Mikulew</td>
+                <td>Front-end developer</td>
+                <td>"Where is my coffee?"</td>
+                <td>
+                    <button type="button" class="btn btn-outline-primary">Button</button>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+</template>
--- src/views/list/SuperheroesList.vue
+++ src/views/list/SuperheroesList.vue
@@ -3,15 +3,21 @@
         <div class="col-12">
             <SearchBar />
         </div>
+
+        <div class="col-12">
+            <VTable />
+        </div>
     </div>
 </template>
 
 <script>
 import SearchBar from '../../components/SearchBar.vue';
+import VTable from '../../components/VTable.vue';
 
 export default {
     components: {
         SearchBar,
+        VTable,
     },
 }
 </script>
