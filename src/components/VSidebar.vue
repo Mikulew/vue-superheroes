28cf358864ad071b0d83cbe33638b8e1861bda6b
Mikulew <mikulew@gmail.com>
2019-03-25 00:21:17 +0100

    Make VSidebar vartically

--- src/components/VSidebar.vue
+++ src/components/VSidebar.vue
@@ -1,33 +1,39 @@
 <template>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <p class="navbar-brand">Superheroe's details #{{id}}</p>
-        <div class="navbar-collapse">
-            <div class="navbar-nav">
+    <nav class="navbar flex-column">
+        <p class="sidebar-title">Superheroe's details #{{id}}</p>
+        <ul class="nav flex-column">
+            <li class="nav-item">
                 <router-link :to="`/${id}/biography`"
-                             class="nav-item nav-link"
+                             class="nav-link"
                              active-class="active">
                              <span>Biography</span>
                 </router-link>
+            </li>
 
+			<li class="nav-item">
                 <router-link :to="`/${id}/appearance`"
-                             class="nav-item nav-link"
+                             class="nav-link"
                              active-class="active">
                             <span>Appearance</span>
                 </router-link>
+			</li>
 
+			<li class="nav-item">
                 <router-link :to="`/${id}/work`"
-                             class="nav-item nav-link"
+                             class="nav-link"
                              active-class="active">
                              <span>Work</span>
                 </router-link>
+			</li>
 
+			<li class="nav-item">
                 <router-link :to="`/${id}/connections`"
-                             class="nav-item nav-link"
+                             class="nav-link"
                              active-class="active">
                              <span>Connections</span>
                 </router-link>
-            </div>
-        </div>
+            </li>
+        </ul>
     </nav>
 </template>
 
@@ -41,3 +47,7 @@ export default {
     }
 }
 </script>
+
+<style scoped>
+
+</style>
