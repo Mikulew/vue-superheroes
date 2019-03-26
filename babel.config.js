ef3519c1085281e79bcdecb8f2447aac2c0fb941
Mikulew <mikulew@gmail.com>
2019-03-26 18:08:16 +0100

    Revert for babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -1,13 +1,5 @@
 module.exports = {
   presets: [
 		'@vue/app',
-		["env", { "modules": false }]
 	],
-	"env": {
-    "test": {
-      "presets": [
-        ["env", { "targets": { "node": "current" }}]
-      ]
-		}
-	}
 }
